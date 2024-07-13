b542d449f1288d27a4476833de4a38d1541dd50c
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-07-13 14:59:19 +0200

    Added integration tests for directories

--- tests/zip.rs
+++ tests/zip.rs
@@ -1,7 +1,7 @@
 use corelib::{self, File};
 
 #[test]
-fn metadata_000() {
+fn sample_000() {
     let mut file = File::new("tests/samples/zip/000.zip");
 
     let metadata = corelib::formats::zip::parser::metadata(&mut file);
@@ -21,7 +21,7 @@ fn metadata_000() {
 }
 
 #[test]
-fn metadata_001() {
+fn sample_001() {
     let mut file = File::new("tests/samples/zip/001.zip");
 
     let metadata = corelib::formats::zip::parser::metadata(&mut file);
@@ -66,4 +66,60 @@ fn metadata_001() {
     );
 
     std::fs::remove_dir_all("tests/samples/zip/001").unwrap();
+}
+
+#[test]
+fn sample_002() {
+    let mut file = File::new("tests/samples/zip/002.zip");
+
+    let metadata = corelib::formats::zip::parser::metadata(&mut file);
+    assert_eq!(metadata.files.len(), 3);
+    assert_eq!(metadata.files[0].file.path, "test/");
+    assert_eq!(metadata.files[0].file.size, 0);
+    assert_eq!(metadata.files[0].compression, "stored");
+    assert_eq!(metadata.files[0].uncompressed_size, 0);
+    assert_eq!(
+        metadata.files[0].file.modified.to_rfc3339(),
+        "2024-07-13T14:27:00+00:00"
+    );
+    assert_eq!(metadata.files[1].file.path, "test/test.txt");
+    assert_eq!(metadata.files[1].file.size, 14);
+    assert_eq!(metadata.files[1].compression, "stored");
+    assert_eq!(metadata.files[1].uncompressed_size, 14);
+    assert_eq!(
+        metadata.files[1].file.modified.to_rfc3339(),
+        "2024-07-13T14:26:48+00:00"
+    );
+    assert_eq!(metadata.files[2].file.path, "test.txt");
+    assert_eq!(metadata.files[2].file.size, 14);
+    assert_eq!(metadata.files[2].compression, "stored");
+    assert_eq!(metadata.files[2].uncompressed_size, 14);
+    assert_eq!(
+        metadata.files[2].file.modified.to_rfc3339(),
+        "2024-07-13T14:26:48+00:00"
+    );
+
+    let test_txt = corelib::formats::zip::parser::get_file(&mut file, &metadata.files[1]);
+    assert_eq!(String::from_utf8(test_txt).unwrap(), "Hello, world!\n");
+    let test_test_txt = corelib::formats::zip::parser::get_file(&mut file, &metadata.files[2]);
+    assert_eq!(String::from_utf8(test_test_txt).unwrap(), "Hello, world!\n");
+
+    std::fs::create_dir_all("tests/samples/zip/002").unwrap();
+
+    corelib::formats::zip::parser::extract(&mut file, metadata.files, 1024, &|path| {
+        format!("tests/samples/zip/002/{}", path)
+    });
+
+    let extracted_test_txt = std::fs::read("tests/samples/zip/002/test.txt").unwrap();
+    assert_eq!(
+        String::from_utf8(extracted_test_txt).unwrap(),
+        "Hello, world!\n"
+    );
+    let extracted_test2_txt = std::fs::read("tests/samples/zip/002/test/test.txt").unwrap();
+    assert_eq!(
+        String::from_utf8(extracted_test2_txt).unwrap(),
+        "Hello, world!\n"
+    );
+
+    std::fs::remove_dir_all("tests/samples/zip/002").unwrap();
 }
