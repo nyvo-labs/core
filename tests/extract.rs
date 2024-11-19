5c2a957bf3511b2e07539a544e68a26ca2fae08a
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-11-19 16:42:09 +0100

    fix: fixed ci

--- tests/extract.rs
+++ tests/extract.rs
@@ -1,22 +1,32 @@
 use acridotheres::{archive::extract, formats::Format};
 use dh::recommended::*;
+use rici::rici;
 use std::path::Path;
 
 #[test]
 fn zip_001() {
+    let ci = rici();
+
+    let output = Path::new(if ci {
+        std::fs::create_dir("output").unwrap();
+        "output/001-zip"
+    } else {
+        "tests/output/001-zip"
+    });
+
     extract::extract_all(
         Path::new("tests/samples/001.zip"),
-        Path::new("tests/output/001-zip"),
+        output,
         Format::Zip,
         1024,
     )
     .unwrap();
 
-    assert!(Path::new("tests/output/001-zip/test.txt").exists());
-    assert!(Path::new("tests/output/001-zip/test2.txt").exists());
+    assert!(output.join("test.txt").exists());
+    assert!(output.join("test2.txt").exists());
 
-    let mut test_txt = dh::file::open_r(Path::new("tests/output/001-zip/test.txt")).unwrap();
-    let mut test2_txt = dh::file::open_r(Path::new("tests/output/001-zip/test2.txt")).unwrap();
+    let mut test_txt = dh::file::open_r(output.join("test.txt")).unwrap();
+    let mut test2_txt = dh::file::open_r(output.join("test2.txt")).unwrap();
 
     assert_eq!(test_txt.read_utf8_at(0, 14).unwrap(), "Hello, world!\n");
     assert_eq!(test2_txt.read_utf8_at(0, 16).unwrap(), "Hello, world! 2\n");
@@ -24,5 +34,5 @@ fn zip_001() {
     test_txt.close().unwrap();
     test2_txt.close().unwrap();
 
-    std::fs::remove_dir_all("tests/output/001-zip").unwrap();
+    std::fs::remove_dir_all(output).unwrap();
 }
